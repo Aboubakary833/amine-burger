3b27c79c1bc48d0f47df3c22c319a1ac1ad12c18
Aboubakary833 <aboubakarycisse410@gmail.com>
2022-02-18 19:19:45 +0000

    Made nav responsive and add Localisation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import logo from '../logo.png'
 import { BiUser } from 'react-icons/bi'
 import { BsBag } from 'react-icons/bs'
+import {HiMenuAlt2} from 'react-icons/hi'
 
 const Nav = () => {
+    const [showNav, setShowNav] = useState(false)
     return (
         <div className='container mt-1 d-flex justify-content-between align-items-center'>
             <div className="logo_and_links w-75 d-flex align-items-center">
-                <div className="logofield">
+                <div className="logofield d-flex d-md-block">
+                    <button className='border-0 outline-0 bg-none d-md-none' onClick={showOrHide}>
+                        <HiMenuAlt2 />
+                    </button>
                     <Link to='/'>
                         <img src={logo} alt="AmineBurger" />
                     </Link>
                 </div>
-                <div className="linksfield d-none d-md-flex justify-content-evenly align-items-center">
+                <div className={`linksfield ${showNav ? 'd-flex' : 'd-none d-md-flex'} flex-column flex-md-row justify-content-evenly align-items-center`}>
                     <NavLink to='/' className={({isActive}) => isActive ? 'active' : null}>Acceuil</NavLink>
                     <NavLink to='/menus' className={({isActive}) => isActive ? 'active' : null}>Menus</NavLink>
                     <NavLink to='/about' className={({isActive}) => isActive ? 'active' : null}>À propos</NavLink>
@@ -30,6 +35,10 @@ const Nav = () => {
             </div>
         </div>
     );
+
+    function showOrHide() {
+        setShowNav(!showNav)
+    }
 }
 
 export default Nav;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,9 +5,17 @@ const About = () => {
         <div className='mt-0 mt-md-4'>
             <div className="about container mt-4 mt-md-0">
                 <h1 className='text-center'>À propos de nous</h1>
-                <div className='mt-4'>
+                <div className='mt-3 content'>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, officia culpa modi aliquid alias enim! Totam, ullam repellendus? Eos repudiandae libero unde quaerat quam. Totam tempora minima itaque debitis dicta!
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore et velit inventore, iusto quaerat minima facere ratione deleniti itaque atque minus eaque fugiat at! Maiores quia nam minus dolorum hic!</p>
+                    <div className='mt-3'>
+                        <h1 className='text-center'>Localisation</h1>
+                        <div className="mt-3 col-10 col-md-8 mx-auto">
+                            <div className="gmap_canvas">
+                                <iframe title='Map du restaurant Amine' id="gmap_canvas" src="https://maps.google.com/maps?q=Centre-ville&t=&z=13&ie=UTF8&iwloc=&output=embed" frameBorder={0} scrolling="no" marginHeight={0} marginWidth={0}></iframe>
+                            </div>
+                        </div>
+                    </div>
                     <p className="mt-3 text-center">&copy;Copyright-AmineBurger-2022</p>
                 </div>
                 
